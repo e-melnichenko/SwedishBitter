dfe927b9fa407f4f3f0da6d249ac1688004ef128
Egor Melnichenko <egor.melnichenko@bk.ru>
2019-11-08 01:51:04 +0300

    production version is ready

--- gulpfile.js
+++ gulpfile.js
@@ -10,13 +10,35 @@ const debug = require('gulp-debug');
 const rename = require('gulp-rename');
 const newer = require('gulp-newer');
 const notify = require('gulp-notify');
+const gulpIf = require('gulp-if');
+const csso = require('gulp-csso');
+const autoprefixer = require('autoprefixer');
+const postcss = require('gulp-postcss');
+const del = require('del');
+const webp = require('gulp-webp');
+const imagemin = require('gulp-imagemin');
 const browserSync = require('browser-sync').create();
+
+const isDevelopment = !process.env.NODE_ENV || process.env.NODE_ENV === 'development';
+
+function isChangeIndexHtml(file) {
+  if(file.basename === 'index.html' && !isDevelopment) return true;
+
+}
 
 
 gulp.task('assets', function() {
   return gulp.src('source/assets/**/*.*', {since: gulp.lastRun('assets')})
+    .pipe(gulpIf(isChangeIndexHtml, through2(function (file, enc, cb) {
+      console.log(isChangeIndexHtml(file));
+      file.contents = replace(file.contents, 'href="style.css"', 'href="style.min.css"');
+      console.log(file.stat.mtime);
+      file.stat.mtime = new Date();
+      console.log(file.stat.mtime);
+      cb(null, file);
+    })))
+    // .pipe(newer('build'))
     .pipe(debug({title: 'assets'}))
-    .pipe(newer('build'))
     .pipe(gulp.dest('build'))
 });
 
@@ -24,13 +46,17 @@ gulp.task('styles', function() {
 
   return combiner(
     gulp.src('source/styles/style.scss'),
-    sourcemaps.init(),
+    gulpIf(isDevelopment, sourcemaps.init()),
     sass(),
     through2(function (file, enc, cb) {
       file.contents = replace(file.contents, 'background-image: url("./', 'background-image: url("img/');
       cb(null, file);
     }),
-    sourcemaps.write(),
+    postcss([autoprefixer()]),
+    gulpIf(isDevelopment, sourcemaps.write(), combiner(
+      csso(),
+      rename("style.min.css"),
+    )),
     debug({title: 'styles'}),
     gulp.dest('build')
   ).on('error', notify.onError(function(err) {
@@ -43,13 +69,30 @@ gulp.task('styles', function() {
 });
 
 gulp.task('assets:styles', function() {
-  return gulp.src('source/styles/**/*.png', {since: gulp.lastRun('assets:styles')})
+  return gulp.src('source/styles/**/*.{png,jpg,webp}', {since: gulp.lastRun('assets:styles')})
     .pipe(rename( function(path) {
       path.dirname = '.';
     }))
     .pipe(newer('build/img'))
+    .pipe(imagemin([
+      imagemin.optipng({optimizationLevel: 3}),
+      imagemin.jpegtran({progressive: true})
+    ]))
     .pipe(debug({title: 'assets:styles'}))
     .pipe(gulp.dest('build/img'))
+});
+
+gulp.task('clear', function(cb) {
+  if(isDevelopment) {
+    cb()} else  {
+      return del('build');
+    }
+});
+
+gulp.task('webp', function() {
+  return gulp.src('source/styles/**/*.{png,jpg}')
+    .pipe(webp({quality: 100}))
+    .pipe(gulp.dest('source/styles'))
 });
 
 gulp.task('server', function() {
@@ -66,5 +109,5 @@ gulp.task('watch', function() {
   gulp.watch('source/styles/**/*.png', gulp.series('assets:styles'))
 });
 
-gulp.task('build', gulp.parallel('styles', 'assets', 'assets:styles'));
+gulp.task('build', gulp.series('clear', gulp.parallel('styles', 'assets', 'assets:styles')));
 gulp.task('dev', gulp.series('build', gulp.parallel('watch', 'server')));
